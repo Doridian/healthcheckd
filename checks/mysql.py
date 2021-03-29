e19387dcfc9489867416362d50ca7809a49ed0db
Mark Dietzer <git@doridian.net>
2021-03-28 19:55:54 -0700

    implement mysql check

--- checks/mysql.py
+++ checks/mysql.py
@@ -1,4 +1,5 @@
 from .base import BaseCheck
+from mysql import connector
 
 presets = {
     'wsrep': {
@@ -19,9 +20,17 @@ class MySQLCheck(BaseCheck):
         self.port = config.getint('port', 3306)
         self.user = config.get('user', 'root')
         self.password = config.get('password', '')
+        self.database = config.get('database', 'INFORMATION_SCHEMA')
 
         presetStr = config.get('preset', 'simple')
         preset = presets[presetStr]
 
         self.query = config.get('query', preset['query'])
         self.expect = config.get('expect', preset['expect'])
+
+    def check(self):
+        conn = connector.connect(host=self.host, user=self.user, passwd=self.password, db=self.database)
+        cur = conn.cursor()
+        cur.execute(self.query)
+        res = cur.fetchone()
+        return str(res) == self.expect
